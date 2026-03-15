48ad724961b61d8fda3193235ff78bedf4891987
Soumik <57496021+MrV3nomous@users.noreply.github.com>
2026-03-16 05:16:33 +0530

    Update starSystem.js

--- src/world/starSystem.js
+++ src/world/starSystem.js
@@ -10,7 +10,7 @@ export class StarSystem {
             x,
             y,
             z,
-            size: 5 + Math.random() * 3,
+            size: 3 + Math.random() * 2,
             type: starTypes[Math.floor(Math.random() * starTypes.length)],
             seed: Math.random() * Math.PI * 2
         };
